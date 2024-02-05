0f5a1c90a25d99aa4e094096d8029b7bf79a6d41
musta.akim <musta.akim@gmail.com>
2024-02-06 06:16:37 +0700

    route and routes

--- feedbackUI/src/components/Post.jsx
+++ feedbackUI/src/components/Post.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 function Post() {
   const params = useParams();
+  const status = 200;
+
+  const onClick = () => {
+    console.log("clicked");
+  };
+
+  if (status === 404) {
+    return <Navigate to="/notfound" />;
+  }
+
   return (
     <div>
       <h1>Post {params.id}</h1>
